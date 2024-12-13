71a27554af2bfbb0f483bc8063027b2346f231ea
luisZamudio7830 <equipo--desarrollo@outlook.com>
2024-12-12 22:26:46 -0300

    primera subida - arquitectura del proyecto

---
+++ actividad-dos-app-escritorio/dominio/Articulo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Articulo
+    {
+        private int idArticulo;
+        private string nombreArticulo;
+        private string descripcion;
+        private Marca marca;
+        private Categoria categoria;
+        private float precio;
+
+        public int MyProperty { get; set; }
+    }
+}
---
+++ actividad-dos-app-escritorio/dominio/Marca.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dominio
+{
+    public class Marca
+    {
+        private int codigoMarca;
+        private string nombreMarca;
+
+        public int MyProperty { get; set; }
+
+    }
+}
